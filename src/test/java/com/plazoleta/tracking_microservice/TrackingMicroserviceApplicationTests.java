91930abe961bbda5601bfeb42bd7792aedee7e74
Edgar J. Salas <ejsalasdev@gmail.com>
2025-07-11 21:59:27 -0500

    initial commit

---
+++ src/test/java/com/plazoleta/tracking_microservice/TrackingMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plazoleta.tracking_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackingMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
